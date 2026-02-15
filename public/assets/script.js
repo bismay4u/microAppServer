7bf3da3457796a7226eedf9933c7c34790dbb5db
Bismay M <bismay@smartinfologks.com>
2026-02-15 15:19:07 +0530

    Local Storage

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,3 +1,62 @@
+const DB_NAME = 'hyperAppsStorage';
+
+function _app_id() {
+    return window.location.pathname.split("/")[1];
+}
+
 function _path_api() {
-    return `${window.location.origin}/api${window.location.pathname.substr(0, window.location.pathname.length-1)}`;
+    return `${window.location.origin}/api/${_app_id()}`;
+}
+
+function openDB(STORE_NAME) {
+    if(!STORE_NAME) STORE_NAME = _app_id();
+    
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(DB_NAME, 1);
+
+        request.onupgradeneeded = () => {
+        const db = request.result;
+        if (!db.objectStoreNames.contains(STORE_NAME)) {
+            db.createObjectStore(STORE_NAME);
+        }
+        };
+
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+}
+
+async function idbSet(key, value) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    const store = tx.objectStore(STORE_NAME);
+    store.put(value, key);
+
+    tx.oncomplete = () => resolve(true);
+    tx.onerror = () => reject(tx.error);
+  });
+}
+
+async function idbGet(key) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readonly');
+    const store = tx.objectStore(STORE_NAME);
+    const req = store.get(key);
+
+    req.onsuccess = () => resolve(req.result);
+    req.onerror = () => reject(req.error);
+  });
+}
+
+async function idbDelete(key) {
+  const db = await openDB();
+  return new Promise((resolve, reject) => {
+    const tx = db.transaction(STORE_NAME, 'readwrite');
+    tx.objectStore(STORE_NAME).delete(key);
+
+    tx.oncomplete = () => resolve(true);
+    tx.onerror = () => reject(tx.error);
+  });
 }
