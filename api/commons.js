3c2704c3b4ad6bf1a09322ad63fe06da1a6cd070
Bismay M <bismay@smartinfologks.com>
2026-02-15 20:43:40 +0530

    node-cache

--- api/commons.js
+++ api/commons.js
@@ -1,5 +1,23 @@
 //All common and global utilities and functions can be defined here
 
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({ stdTTL: process.env.CACHE_TTL || 300 });
+
 module.exports = function(app) {
 
+}
+
+global._CACHE = {
+    set: function(key, value, ttl) {
+        myCache.set(key,value,ttl);
+    },
+
+    get: function(key) {
+        return myCache.get(key);
+    },
+
+    has: function(key) {
+        const result =  myCache.has(key);
+        return result;
+    }
 }
